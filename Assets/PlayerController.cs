4bfcaa12a274c265601a481ab29e1640e5665d6e
BadMojo91 <badmojo91@gmail.com>
2020-03-02 07:55:05 +1030

    Initial project setup
    nothing to see here

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 targetPosition;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            GotoClickPoint();
+    }
+
+    private void GotoClickPoint()
+    {
+        Vector3 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mPos.z = 0;
+        targetPosition = mPos;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(targetPosition, .1f);
+    }
+}
