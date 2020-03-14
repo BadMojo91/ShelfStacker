c2a791c139c700aec2a08f09a1f477a3c7dbc850
BadMojo91 <badmojo91@gmail.com>
2020-03-14 22:57:41 +1030

    Artistic design change from 2D isometric to full 3D
    +Basic fixture and CanOSoup model
    +Added product script and bay script, can also fill and remove products
    +Added basic player movement script, really shit but it works
    *Cant confine mouse cursor to window.. possibly windows virtual desktop interfearing, could be nothing
    
    Keeping old 2d resources, might be good for the UI or minimap

---
+++ Assets/Bay.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class Bay
+{
+    public Product product;
+    public int maxProductsDepth;
+    public int maxProductsWidth;
+    public Vector3 boundingBoxSize;
+    public Vector3 boundingBoxOffset;
+    public float gridScale;
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Player stuff
+    public GameObject player;
+
+    public MouseLook mLook;
+
+    //Movement
+    public float walkSpeed, runSpeed, jumpForce, gravity, maxHeight;
+
+    private float oldHeight, oldForce;
+    private bool isJumping;
+    private CharacterController controller;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void Update()
+    {
+        mLook.MouseUpdate(transform, Camera.main.transform);
+
+        //movement
+        MoveUpdate();
+
+        if (Input.GetButtonDown("Fire1"))
+            Use();
+    }
+
+    private void Use()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hit))
+        {
+            Debug.Log(hit.collider);
+            if (hit.collider.GetComponent<Fixture>())
+            {
+                Debug.Log("eh");
+                hit.collider.GetComponent<Fixture>().RemoveItem(transform);
+            }
+        }
+    }
+
+    private void MoveUpdate()
+    {
+        Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        dir = transform.TransformDirection(dir);
+        //dir *= Input.GetButton("Run") ? runSpeed : walkSpeed;
+        dir *= walkSpeed;
+        //====== jump  ========
+        if (controller.isGrounded && Input.GetButton("Jump"))
+        {
+            oldHeight = maxHeight; //get current height plus max height
+            isJumping = true;
+        }
+        oldHeight -= Time.deltaTime;
+        if (isJumping && oldHeight > 0)
+        {
+            oldForce = jumpForce;
+        }
+        if (isJumping && transform.position.y >= oldHeight)
+        {
+            isJumping = false;
+        }
+        else if (isJumping && Physics.CheckSphere(transform.position + Vector3.up, 1f, 1, QueryTriggerInteraction.Ignore))
+        {
+            isJumping = false;
+            oldForce = 0;
+        }
+
+        if (!isJumping && oldForce > 0f)
+        {
+            oldForce -= Time.deltaTime;
+        }
+        //=====================
+
+        oldForce -= gravity * Time.deltaTime;
+        dir.y = oldForce;
+        controller.Move(dir * Time.deltaTime);
+    }
+}
